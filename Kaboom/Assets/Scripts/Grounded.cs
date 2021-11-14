a45bab97d701ed2bf71e460b7762670be8728caa
Gabriel Rodriguez <75849371+Garelion@users.noreply.github.com>
2021-11-14 08:24:53 -0500

    Fixing Main File
    Made a mistake, fixed and added missing files.

---
+++ Kaboom/Assets/Scripts/Grounded.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grounded : MonoBehaviour
+{
+    GameObject player;
+    void Start()
+    {
+        player = transform.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Ground")
+        {
+            player.GetComponent<PlayerController>().isGrounded = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Ground")
+        {
+            player.GetComponent<PlayerController>().isGrounded = false;
+        }
+    }
+}
